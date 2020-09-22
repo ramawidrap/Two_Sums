312a1b1b6e6b95c87645be42b7b2a05f78530686
ramaputra <rama@jojonomic.com>
2020-09-22 18:00:58 +0700

    add two sums

---
+++ twoSums.py
@@ -0,0 +1,34 @@
+import sys
+
+def twoSums(list,sum):
+    pair = []
+    hashTable = {}
+    for index, value in enumerate(list):
+        complement = sum - value
+        if complement not in hashTable:
+            hashTable[value] = index
+        else:
+            pair = [hashTable[complement],index]
+
+    if(len(pair) == 0):
+        return "output tidak ditemukan"
+    elif(len(pair) == 2):
+        return pair
+    else:
+        return "Invalid"
+
+
+if __name__ == "__main__":
+    if(len(sys.argv) == 3):
+        input = sys.argv[3][1,len(sys.argv-1)].split(",")
+        sum = sys.argv[4]
+        output = twoSums(input,sum)
+        print(output)
+    elif(len(sys.argv) == 1):
+        listInput = [[2,7,11,15],[3,2,4],[3,3]]
+        sum = [13,6,6]
+        for x in range(3):
+            output = twoSums(listInput[x],sum[x])
+            print(output)
+    else:
+        print("Argument tidak valid")
